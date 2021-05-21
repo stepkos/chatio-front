f2ee35a1d8310a6c39079d47a605f773bb8d219f
stepkos <stepkos@wp.pl>
2021-05-21 16:10:55 +0200

    RWD

--- src/pages/ChatRWD.js
+++ src/pages/ChatRWD.js
@@ -5,31 +5,12 @@ import Chat from './../layout/Chat';
 
 
 const ChatRWD = () => {
-
-	const [smallVersion, setSmallVersion] = useState(window.innerWidth < 1050);
-
-	window.addEventListener('resize', () => {
-		let sv = window.innerWidth < 1050;
-		
-		if (sv !== smallVersion) 
-			setSmallVersion(sv);
-	});
-
+	
 	return (
 		<>
-			{
-				(smallVersion) ?
-				<>
-					<Conversations />
-					<Chat />
-				</>
-				:
-				<>
-					<LeftMenu />
-					<Conversations />
-					<Chat />
-				</>
-			}
+			<LeftMenu />
+			<Conversations />
+			<Chat />
     	</>
   	);
 }
